13958e41d0484103b0dd616ebe215562499aa4b6
nickbui <nicholas.t.bui@gmail.com>
2021-07-17 23:55:45 -0400

    Calculator client created

--- CalculatorServer/src/Calc/CalculatorClient.java
+++ CalculatorServer/src/Calc/CalculatorClient.java
@@ -5,11 +5,24 @@
  */
 package Calc;
 
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+
 /**
  *
  * @author nickbui
  */
 public class CalculatorClient
 {
-    
+    public static void main(String[] args)
+    {
+        try{
+            Calculator calc = (Calculator)Naming.lookup("rmi://localhost:1999/Calculator");
+            System.out.println(calc.add(10, 5));
+        } catch (RemoteException e){
+            System.out.println("Remote exception: " + e.getMessage());
+        } catch (Exception e){
+            System.out.println("Exception: " + e.getMessage());
+        }
+    }
 }
--- CalculatorServer/src/Calc/Main.java
+++ CalculatorServer/src/Calc/Main.java
@@ -4,11 +4,6 @@ import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
  *
@@ -16,10 +11,6 @@ import java.rmi.registry.Registry;
  */
 public class Main
 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args)
     {
         try{
